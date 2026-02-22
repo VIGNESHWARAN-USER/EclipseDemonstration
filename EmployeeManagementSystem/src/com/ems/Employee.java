4f7d36b49ceb51b637f4d499a2e82daff5d9a838
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-02-22 15:25:10 +0530

    Changes made on Vignesh branch

--- EmployeeManagementSystem/src/com/ems/Employee.java
+++ EmployeeManagementSystem/src/com/ems/Employee.java
@@ -4,6 +4,24 @@ public class Employee {
 	private String empId;
 	private String name;
 	private double salary;
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 	
 	
 }
